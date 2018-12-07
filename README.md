3ad4af76fa923fe09f5ec8e12c175ee8e946c6d7
Planck35 <chaoyuw@andrew.cmu.edu>
2018-12-07 00:29:58 -0800

    update readme

--- README.md
+++ README.md
@@ -14,3 +14,24 @@ Target paper: [Total Style Transfer with a Single Feed-Forward Network](https://
 -   [Arbitrary Style Transfer in Real-time with Adaptive Instance Normalization](https://arxiv.org/pdf/1703.06868.pdf)
     Introduced well performance transform
 
+### Method
+We basically follow the structure of target paper, and try to reproduce the result.
+![](image/structure.png)
+The encoder and decoder pair is a mirror connected VGG-16 with pre-trained weights from ImageNet
+
+The paper consider inter-scale & intra-scale features which is a new perspective.
+
+For the MST, we extract multi-scale content & style features from relu1_2, relu2_2, relu3_3 layers of encoder, upsample and concatenate them into one tensor. 
+
+Flatten the height & width dimension into one dimension, and zero centor that specific dimension. 
+
+At first, we try to normalize the zero center matrix by its covariance matrix. Then, we apply normalization of zero center matrix using its standard deviation along flatten dimension. 
+
+After that, we scale the normalized matrix with style’s standard deviation along flatten dimension and also add style’s flatten dimension mean matrix to get the transform result.
+
+After finishing transform of concatenated tensor, we divided and downsampled the result, and feed relative result into decoder to generate stylized image. We also used skip-connection to improve the result
+
+Loss function consider content loss and one of inter-scale & intra-scale loss. Optimizer is Adam.
+
+### Result
+![](image/result.png)
--- main.py
+++ main.py
@@ -18,19 +18,20 @@ import copy
 MAX_EPOCH = 1
 BATCH_SIZE = 2
 LEARNING_RATE = 1e-4
-CONTENT_PATH = "./data/content_img/" #"./data/content_img/"
-STYLE_PATH = "./data/style_img/" #"./data/style_img/"
+CONTENT_PATH = "./data/content_img/"
+STYLE_PATH = "./data/style_img/"
+
 
 def calc_mean_cov(matrix, eps=1e-5):
     '''[compute the covariance of image]
-    
+
     Arguments:
         cF {[Tensor]} -- [size:batch*3*224*224]
-    
+
     Returns:
         [type] -- [description]
     '''
-    
+
     # # b*c*(h*w)
     # size = matrix.size()
     # N,C = size[:2]
@@ -44,20 +45,22 @@ def calc_mean_cov(matrix, eps=1e-5):
     # #matrix_covariance = torch.bmm(matrix_0_center, matrix_0_center.transpose(1,2))
 
     # #return matrix_mean, matrix_covariance
-    
+
     a, b, c, d = matrix.size()  # a=batch size(=1)
     # b=number of feature maps
     # (c,d)=dimensions of a f. map (N=c*d)
 
     features = matrix.view(a, b, c * d)  # resise F_XL into \hat F_XL
 
-    G = torch.bmm(features, features.transpose(1,2))  # compute the gram product
+    # compute the gram product
+    G = torch.bmm(features, features.transpose(1, 2))
 
     # we 'normalize' the values of the gram matrix
     # by dividing by the number of element in each feature maps.
     return G.div(c*d)
 
     # return m_mean, m_std
+
 
 def calc_style_loss(cF, sF, loss_fn):
     # m1, c1 = calc_mean_cov(cF)
@@ -66,6 +69,7 @@ def calc_style_loss(cF, sF, loss_fn):
     m1 = calc_mean_cov(cF)
     m2 = calc_mean_cov(sF)
     return loss_fn(m1, m2)
+
 
 def upsample_and_cat(content_relu):
     factor2to1 = content_relu[0].size(2)//content_relu[1].size(2)
@@ -76,14 +80,15 @@ def upsample_and_cat(content_relu):
     content_relu[2] = upsample3to1(content_relu[2])
     return torch.cat(content_relu, dim=1)
 
+
 class TotalSytle():
     def __init__(self):
 
         self.train_loader = get_data_loader(
-            content_path = CONTENT_PATH,
-            style_path = STYLE_PATH,
-            batch_size = BATCH_SIZE,
-            small_test = False
+            content_path=CONTENT_PATH,
+            style_path=STYLE_PATH,
+            batch_size=BATCH_SIZE,
+            small_test=False
         )
 
         self.encoder = Encoder()
@@ -94,25 +99,25 @@ class TotalSytle():
         self.mse_loss = torch.nn.MSELoss()
         # self.style_loss = StyleLoss() ## TODO: Complete Styleloss
         # self.content_loss = ContentLoss() ## TODO: Complete ContentLoss
-        print ("----------------------Model is loaded----------------------------")
+        print("----------------------Model is loaded----------------------------")
         parameters = self.decoder.parameters()
         self.optimizer = torch.optim.Adam(parameters, lr=LEARNING_RATE)
 
         self.use_gpu = True
         if self.use_gpu:
-            print ("----------------------GPU is used to train---------------------------")
+            print("----------------------GPU is used to train---------------------------")
             self.encoder.cuda()
             self.decoder.cuda()
             self.mse_loss.cuda()
         else:
-            print ("----------------------CPU is used to train----------------------------")
+            print(
+                "----------------------CPU is used to train----------------------------")
 
-        
-        self.alpha = 0.5 #the weight of content loss and style loss
-        self.beta = 1 # the weight of inter_scale loss and inter_scale loss. 1: only use loss_intra_scale
+        self.alpha = 0.5  # the weight of content loss and style loss
+        self.beta = 1  # the weight of inter_scale loss and inter_scale loss. 1: only use loss_intra_scale
 
     def train(self):
-        
+
         for epoch in range(MAX_EPOCH):
             total_loss = 0
             for batch_id, (style_imgs, content_imgs) in enumerate(self.train_loader):
@@ -120,50 +125,54 @@ class TotalSytle():
                 content_imgs = content_imgs.cuda()
                 self.optimizer.zero_grad()
                 torch.cuda.empty_cache()
-                #Parse the style_imgs and content_imgs into encoder
+                # Parse the style_imgs and content_imgs into encoder
                 encoded_style, output_style = self.encoder(style_imgs)
                 encoded_content, output_content = self.encoder(content_imgs)
                 # e_loss = encoded_content[-1]
-                
+
                 encoded_content_save = copy.deepcopy(encoded_content)
                 encoded_style_save = copy.deepcopy(encoded_style)
-                #Compute the MST transformed relu
+                # Compute the MST transformed relu
                 relu1_2, relu2_2, relu3_3 = MST(encoded_content, encoded_style)
                 # print(relu1_2)
                 # print(relu1_2.size(), relu2_2.size(), relu3_3.size())
-                #Skip connection with decoder
+                # Skip connection with decoder
                 stylized_img = self.decoder(relu1_2, relu2_2, relu3_3)
                 # print(stylized_img.size())
 
-                #Extract the features of generated stylized img
-                
+                # Extract the features of generated stylized img
+
                 encoded_stylized, _ = self.encoder(stylized_img)
-                
+
                 # content_loss, _ = self.encoder(content_imgs)
-                #compute the loss between stylized imgs and content imgs
-                #use only relu3_3 to as the 'content' of an img
-                loss_c = self.mse_loss(encoded_stylized[-1], encoded_content_save[-1])
+                # compute the loss between stylized imgs and content imgs
+                # use only relu3_3 to as the 'content' of an img
+                loss_c = self.mse_loss(
+                    encoded_stylized[-1], encoded_content_save[-1])
 
                 # loss_c = calc_style_loss(encoded_stylized[-1], encoded_content_save[-1], self.mse_loss)
-                #compute the loss between stylized imgs and style imgs
+                # compute the loss between stylized imgs and style imgs
                 # intra scale loss
-                loss_intra_scale = calc_style_loss(encoded_stylized[0], encoded_style_save[0], self.mse_loss)
+                loss_intra_scale = calc_style_loss(
+                    encoded_stylized[0], encoded_style_save[0], self.mse_loss)
                 # loss_intra_scale = self.mse_loss(encoded_stylized[0], encoded_style_save[0])
                 for i in range(1, 3):
-                    loss_intra_scale += calc_style_loss(encoded_stylized[i], encoded_style_save[i], self.mse_loss)
+                    loss_intra_scale += calc_style_loss(
+                        encoded_stylized[i], encoded_style_save[i], self.mse_loss)
                     #loss_intra_scale += self.mse_loss(encoded_stylized[i], encoded_style_save[i])
 
                 # inter scale loss
                 encoded_stylized = upsample_and_cat(encoded_stylized)
                 encoded_content = upsample_and_cat(encoded_content)
-                loss_inter_sacle = calc_style_loss(encoded_stylized, encoded_content, self.mse_loss)
+                loss_inter_sacle = calc_style_loss(
+                    encoded_stylized, encoded_content, self.mse_loss)
 
+                # weighted sum of inter_scale loss and intra scale loss
+                # the default self.bata = 1 for only using intra scale loss
+                loss_s = self.beta * loss_intra_scale + \
+                    (1-self.beta) * loss_inter_sacle
 
-                #weighted sum of inter_scale loss and intra scale loss
-                #the default self.bata = 1 for only using intra scale loss
-                loss_s =  self.beta * loss_intra_scale + (1-self.beta) * loss_inter_sacle
-
-                #weighted sum of style loss and content loss
+                # weighted sum of style loss and content loss
                 # loss = self.alpha * loss_s + (1-self.alpha) * loss_c
                 loss = 0.99 * loss_s + 0.01 * loss_c
                 # print("loss_s-loss_c: ", loss_s.item(), loss_c.item())
@@ -175,18 +184,23 @@ class TotalSytle():
                 generated_img = stylized_img.detach().cpu()
                 for i, gimg in enumerate(generated_img):
                     # gimg = transforms.functional.to_pil_image(gimg)
-                    vutils.save_image(gimg, "./data/generate/" + str(batch_id) + str(i) + ".jpg")
+                    vutils.save_image(
+                        gimg, "./data/generate/" + str(batch_id) + str(i) + ".jpg")
                     # gimg.save("./data/generate/" + str(batch_id) + str(i) + ".jpg")
 
             # generated_img = stylized_img.detach().cpu()
             # generated_img = transforms.functional.to_pil_image(generated_img[0])
             # vutils.save_image(generated_img,"./data/generate/" + str(epoch) + ".jpg")
             # generated_img.save("./data/generate/" + str(epoch) + ".jpg")
-                    
-            print ("[TRAIN] EPOCH %d/%d, Loss/batch_num: %.4f" % (epoch, MAX_EPOCH, total_loss/(batch_id+1)))
-            if epoch%50 == 0:
-                torch.save(self.encoder.state_dict(), "./weights/epoch"+str(epoch)+"_encoder.pt")
-                torch.save(self.decoder.state_dict(), "./weights/epoch"+str(epoch)+"_decoder.pt")
+
+            print("[TRAIN] EPOCH %d/%d, Loss/batch_num: %.4f" %
+                  (epoch, MAX_EPOCH, total_loss/(batch_id+1)))
+            if epoch % 50 == 0:
+                torch.save(self.encoder.state_dict(),
+                           "./weights/epoch"+str(epoch)+"_encoder.pt")
+                torch.save(self.decoder.state_dict(),
+                           "./weights/epoch"+str(epoch)+"_decoder.pt")
+
 
 model = TotalSytle()
 model.train()
